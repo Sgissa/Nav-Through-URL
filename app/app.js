0e947a58373c2294376b4684944d95316e8e9c12
Sgissa <143265121+Sgissa@users.noreply.github.com>
2023-10-18 13:08:41 -0400

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,35 @@
+function changeRoute() {
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#", ""); //removes hashtag
+    console.log(hashTag + " " + pageID); 
+
+    if (pageID != "" ) {
+        $.get(`pages/${pageID}/${pageID}.html`, function(data){
+
+            // console.log("data " + data)
+            $("#app").html(data)
+        })
+
+
+        
+    } else {
+        $.get(`pages/home/home.html`, function(data){
+
+            // console.log("data " + data)
+            $("#app").html(data)
+        })
+    }
+
+    
+
+}
+
+function initURLListener () {
+    $(window).on("hashchange", changeRoute);
+    changeRoute();
+}
+
+$(document).ready(function () {
+    initURLListener();
+    
+});
